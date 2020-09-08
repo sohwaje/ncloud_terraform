19c2e5ee0f400defb4428bb3e9912222980cda59
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-08 19:31:45 +0900

    update

--- install.sh
+++ install.sh
@@ -406,7 +406,7 @@ mkdir -p ~/"${SOURCE_DIR}"/"${CATALINA_BASE_MAME}"; \
 mkdir -p ~/"${SOURCE_DIR}"/"${CATALINA_BASE_MAME}"/logs/gclog
 
 # mysql-connector 복사
-wget -P   ~/"${CATALINA_BASE_MAME}"/lib https://github.com/sohwaje/ncloud_terraform/blob/master/mysql-connector-java-5.1.26-bin.jar
+wget -P ~/"${CATALINA_BASE_MAME}"/lib https://github.com/sohwaje/ncloud_terraform/blob/master/mysql-connector-java-5.1.26-bin.jar
 
 # tomcat start
 "${CATALINA_BASE_MAME}"/bin/startup.sh
