20d07bc3c9ed3dc7ccd234db03453c5934e434b4
sigongweb <kisssulran@i-screammedia.com>
2020-09-22 10:49:48 +0900

    update

--- user-data.sh
+++ user-data.sh
@@ -197,139 +197,139 @@ sudo systemctl daemon-reload && sudo systemctl start node_exporter && sudo syste
 # sudo curl -X POST http://10.1.12.6:9090/-/reload
 
 # Tomcat configuration
-## 변수 설정
-# SOURCE_DIR="webapps"
-# CATALINA_HOME_NAME="apache-tomcat-7.0.90"
-# CATALINA_BASE_NAME="gneerbank"
-# TOMCAT_USER="sigongweb"
-# sudo groupadd $TOMCAT_USER
-# sudo useradd -g $TOMCAT_USER -s /usr/sbin/nologin/ $TOMCAT_USER
-# # Install tomcat7
-# cd /home/$TOMCAT_USER; \
-# sudo wget https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.90/bin/$CATALINA_HOME_NAME.tar.gz; \
-#  sudo tar xvfz "$CATALINA_HOME_NAME".tar.gz; \
-#  sudo cp -ar "$CATALINA_HOME_NAME" "$CATALINA_BASE_NAME"; \
-#  sudo rm -f "$CATALINA_HOME_NAME".tar.gz
-#
-# # 톰캣 환경 변수 설정
-# sudo bash -c "echo 'export CATALINA_BASE=/home/$TOMCAT_USER/$CATALINA_BASE_NAME' >> /home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
-# sudo bash -c "echo 'export CATALINA_HOME=/home/$TOMCAT_USER/$CATALINA_HOME_NAME' >> /home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
-#
-# echo '''export DATE=`date +%Y%m%d%H%M%S`
-# #[2] TOMCAT Port & values
-# # Tomcat Port 설정
-# export PORT_OFFSET=0
-# export HTTP_PORT=$(expr 8080 + $PORT_OFFSET)
-# export AJP_PORT=$(expr 8009 + $PORT_OFFSET)
-# export SSL_PORT=$(expr 8443 + $PORT_OFFSET)
-# export SHUTDOWN_PORT=$(expr 8005 + $PORT_OFFSET)
-#
-# # Tomcat Threads 설정
-# export JAVA_OPTS="$JAVA_OPTS -DmaxThreads=300"
-# export JAVA_OPTS="$JAVA_OPTS -DminSpareThreads=50"
-# export JAVA_OPTS="$JAVA_OPTS -DacceptCount=10"
-# export JAVA_OPTS="$JAVA_OPTS -DmaxKeepAliveRequests=-1"
-# export JAVA_OPTS="$JAVA_OPTS -DconnectionTimeout=30000"
-#
-# #[4] Directory Setup #####
-# export SERVER_NAME=gneerbank
-# export JAVA_OPTS="$JAVA_OPTS -Dserver=gneerbank"
-# export JAVA_HOME="/etc/alternatives/jre_1.8.0_openjdk"
-# export LOG_HOME=$CATALINA_BASE/logs
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CATALINA_HOME/lib
-# # export SCOUTER_AGENT_DIR="/home/sigongweb/work/agent.java"
-#
-# #[5] JVM Options : Memory
-# export JAVA_OPTS="$JAVA_OPTS -Xms4096m"
-# export JAVA_OPTS="$JAVA_OPTS -Xmx4096m"
-# export JAVA_OPTS="$JAVA_OPTS -XX:NewSize=256m"
-# export JAVA_OPTS="$JAVA_OPTS -Xss512k"
-#
-# #[6] Parallel GC OPTIONS ###
-# export JAVA_OPTS="$JAVA_OPTS -XX:+UseParallelOldGC "
-# export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=\"utf-8\""
-#
-# #[7] JVM Option GCi log, Stack Trace, Dump
-# export JAVA_OPTS="$JAVA_OPTS -verbose:gc"
-# export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCTimeStamps"
-# export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails "
-# export JAVA_OPTS="$JAVA_OPTS -Xloggc:$LOG_HOME/gclog/gc_$DATE.log"
-# export JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
-# export JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=$LOG_HOME/gclog/java_pid.hprof"
-# export JAVA_OPTS="$JAVA_OPTS -XX:+DisableExplicitGC"
-# export JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
-# export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
-#
-# export JAVA_OPTS="$JAVA_OPTS -Dhttp.port=$HTTP_PORT"
-# export JAVA_OPTS="$JAVA_OPTS -Dajp.port=$AJP_PORT"
-# export JAVA_OPTS="$JAVA_OPTS -Dssl.port=$SSL_PORT"
-# export JAVA_OPTS="$JAVA_OPTS -Dshutdown.port=$SHUTDOWN_PORT"
-# export JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$CATALINA_HOME/lib/"
-# export JAVA_OPTS
-# echo "================================================"
-# echo "JAVA_HOME=$JAVA_HOME"
-# echo "CATALINA_HOME=$CATALINA_HOME"
-# echo "SERVER_HOME=$CATALINA_BASE"
-# echo "HTTP_PORT=$HTTP_PORT"
-# echo "SSL_PORT=$SSL_PORT"
-# echo "AJP_PORT=$AJP_PORT"
-# echo "SHUTDOWN_PORT=$SHUTDOWN_PORT"
-# echo "================================================"
-# ''' | sudo tee -a "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
-# sudo chmod +x "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
-#
-# # server.xml 복사
-# sudo rm -f "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf/server.xml"
-# sudo wget -P \
-#   "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf" https://raw.githubusercontent.com/sohwaje/ncloud_terraform/master/server.xml
-#
-# # tomcat database 설정
-# sudo mkdir -p "/home/$TOMCAT_USER/$SOURCE_DIR/$CATALINA_BASE_NAME"; \
-#  sudo mkdir -p "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf/Catalina/localhost"; \
-# echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-#   <!-- 1. 소스 경로 -->
-# <Context path=\"\" docBase="\"/home/$TOMCAT_USER"/"$SOURCE_DIR"/"$CATALINA_BASE_NAME"\" reloadable=\"false\"
-#          privileged=\"true\" antiResourceLocking=\"false\" antiJARLocking=\"false\">
-# <!-- 2. DB 정보 -->
-#     <Resource name=\"jdbc/elLetterMDS\" auth=\"Container\"
-#               type=\"javax.sql.DataSource\"
-#               driverClassName=\"com.mysql.jdbc.Driver\"
-#               validationQuery=\"SELECT 1\"
-#               validationInterval=\"30000\"
-#               url=\"jdbc:mysql://10.1.3.4:3306/hiclass_stage_db?useUnicode=true&amp;characterEncoding=UTF-8&amp;characterSetResults=UTF-8&amp;useSSL=true&amp;serverTimezone=Asia/Seoul\"
-#               username=\"class_user_stage\"
-#               password=\"class@1904\"
-#               maxActive=\"100\" maxIdle=\"50\" initialSize=\"30\" maxWait=\"-1\"/>
-# </Context>" | sudo tee -a "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf/Catalina/localhost/ROOT.xml"
-#
-# # gclog 디렉토리 생성
-# sudo mkdir -p "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/logs/gclog"
-#
-# # mysql-connector 복사
-# sudo curl -L "https://github.com/sohwaje/ncloud_terraform/blob/master/mysql-connector-java-8.0.21.jar?raw=true" -o "/home/$TOMCAT_USER/$CATALINA_HOME_NAME/lib/mysql-connector-java-8.0.21.jar"
-#
-# # Change permission tomcat Directory
-# sudo chown -R $TOMCAT_USER:$TOMCAT_USER /home/$TOMCAT_USER
-#
-# # add systemctl tomcat service
-# sudo bash -c "cat << EOF > /etc/systemd/system/tomcat.service
-# [Unit]
-# Description=tomcat7
-# After=network.target syslog.target
-#
-# [Service]
-# Type=forking
-# User=$TOMCAT_USER
-# Group=$TOMCAT_USER
-# ExecStart=/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/startup.sh
-# ExecStop=/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/shutdown.sh
-#
-# [Install]
-# WantedBy=multi-user.target
-# EOF"
-#
-# # Create test index.jsp
-# echo "NCloud TEST PAGE-$HOSTNAME" | sudo tee -a "/home/$TOMCAT_USER/$SOURCE_DIR/$CATALINA_BASE_NAME/index.jsp"
-#
-# # tomcat start
-# sudo systemctl daemon-reload && sudo systemctl start tomcat && sudo systemctl enable tomcat
+# 변수 설정
+SOURCE_DIR="webapps"
+CATALINA_HOME_NAME="apache-tomcat-7.0.90"
+CATALINA_BASE_NAME="gneerbank"
+TOMCAT_USER="sigongweb"
+sudo groupadd $TOMCAT_USER
+sudo useradd -g $TOMCAT_USER -s /usr/sbin/nologin/ $TOMCAT_USER
+# Install tomcat7
+cd /home/$TOMCAT_USER; \
+sudo wget https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.90/bin/$CATALINA_HOME_NAME.tar.gz; \
+ sudo tar xvfz "$CATALINA_HOME_NAME".tar.gz; \
+ sudo cp -ar "$CATALINA_HOME_NAME" "$CATALINA_BASE_NAME"; \
+ sudo rm -f "$CATALINA_HOME_NAME".tar.gz
+
+# 톰캣 환경 변수 설정
+sudo bash -c "echo 'export CATALINA_BASE=/home/$TOMCAT_USER/$CATALINA_BASE_NAME' >> /home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
+sudo bash -c "echo 'export CATALINA_HOME=/home/$TOMCAT_USER/$CATALINA_HOME_NAME' >> /home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
+
+echo '''export DATE=`date +%Y%m%d%H%M%S`
+#[2] TOMCAT Port & values
+# Tomcat Port 설정
+export PORT_OFFSET=0
+export HTTP_PORT=$(expr 8080 + $PORT_OFFSET)
+export AJP_PORT=$(expr 8009 + $PORT_OFFSET)
+export SSL_PORT=$(expr 8443 + $PORT_OFFSET)
+export SHUTDOWN_PORT=$(expr 8005 + $PORT_OFFSET)
+
+# Tomcat Threads 설정
+export JAVA_OPTS="$JAVA_OPTS -DmaxThreads=300"
+export JAVA_OPTS="$JAVA_OPTS -DminSpareThreads=50"
+export JAVA_OPTS="$JAVA_OPTS -DacceptCount=10"
+export JAVA_OPTS="$JAVA_OPTS -DmaxKeepAliveRequests=-1"
+export JAVA_OPTS="$JAVA_OPTS -DconnectionTimeout=30000"
+
+#[4] Directory Setup #####
+export SERVER_NAME=gneerbank
+export JAVA_OPTS="$JAVA_OPTS -Dserver=gneerbank"
+export JAVA_HOME="/etc/alternatives/jre_1.8.0_openjdk"
+export LOG_HOME=$CATALINA_BASE/logs
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CATALINA_HOME/lib
+# export SCOUTER_AGENT_DIR="/home/sigongweb/work/agent.java"
+
+#[5] JVM Options : Memory
+export JAVA_OPTS="$JAVA_OPTS -Xms4096m"
+export JAVA_OPTS="$JAVA_OPTS -Xmx4096m"
+export JAVA_OPTS="$JAVA_OPTS -XX:NewSize=256m"
+export JAVA_OPTS="$JAVA_OPTS -Xss512k"
+
+#[6] Parallel GC OPTIONS ###
+export JAVA_OPTS="$JAVA_OPTS -XX:+UseParallelOldGC "
+export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=\"utf-8\""
+
+#[7] JVM Option GCi log, Stack Trace, Dump
+export JAVA_OPTS="$JAVA_OPTS -verbose:gc"
+export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCTimeStamps"
+export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails "
+export JAVA_OPTS="$JAVA_OPTS -Xloggc:$LOG_HOME/gclog/gc_$DATE.log"
+export JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
+export JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=$LOG_HOME/gclog/java_pid.hprof"
+export JAVA_OPTS="$JAVA_OPTS -XX:+DisableExplicitGC"
+export JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
+export JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
+
+export JAVA_OPTS="$JAVA_OPTS -Dhttp.port=$HTTP_PORT"
+export JAVA_OPTS="$JAVA_OPTS -Dajp.port=$AJP_PORT"
+export JAVA_OPTS="$JAVA_OPTS -Dssl.port=$SSL_PORT"
+export JAVA_OPTS="$JAVA_OPTS -Dshutdown.port=$SHUTDOWN_PORT"
+export JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$CATALINA_HOME/lib/"
+export JAVA_OPTS
+echo "================================================"
+echo "JAVA_HOME=$JAVA_HOME"
+echo "CATALINA_HOME=$CATALINA_HOME"
+echo "SERVER_HOME=$CATALINA_BASE"
+echo "HTTP_PORT=$HTTP_PORT"
+echo "SSL_PORT=$SSL_PORT"
+echo "AJP_PORT=$AJP_PORT"
+echo "SHUTDOWN_PORT=$SHUTDOWN_PORT"
+echo "================================================"
+''' | sudo tee -a "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
+sudo chmod +x "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/setenv.sh"
+
+# server.xml 복사
+sudo rm -f "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf/server.xml"
+sudo wget -P \
+  "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf" https://raw.githubusercontent.com/sohwaje/ncloud_terraform/master/server.xml
+
+# tomcat database 설정
+sudo mkdir -p "/home/$TOMCAT_USER/$SOURCE_DIR/$CATALINA_BASE_NAME"; \
+ sudo mkdir -p "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf/Catalina/localhost"; \
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  <!-- 1. 소스 경로 -->
+<Context path=\"\" docBase="\"/home/$TOMCAT_USER"/"$SOURCE_DIR"/"$CATALINA_BASE_NAME"\" reloadable=\"false\"
+         privileged=\"true\" antiResourceLocking=\"false\" antiJARLocking=\"false\">
+<!-- 2. DB 정보 -->
+    <Resource name=\"jdbc/elLetterMDS\" auth=\"Container\"
+              type=\"javax.sql.DataSource\"
+              driverClassName=\"com.mysql.jdbc.Driver\"
+              validationQuery=\"SELECT 1\"
+              validationInterval=\"30000\"
+              url=\"jdbc:mysql://10.1.3.4:3306/hiclass_stage_db?useUnicode=true&amp;characterEncoding=UTF-8&amp;characterSetResults=UTF-8&amp;useSSL=true&amp;serverTimezone=Asia/Seoul\"
+              username=\"class_user_stage\"
+              password=\"class@1904\"
+              maxActive=\"100\" maxIdle=\"50\" initialSize=\"30\" maxWait=\"-1\"/>
+</Context>" | sudo tee -a "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/conf/Catalina/localhost/ROOT.xml"
+
+# gclog 디렉토리 생성
+sudo mkdir -p "/home/$TOMCAT_USER/$CATALINA_BASE_NAME/logs/gclog"
+
+# mysql-connector 복사
+sudo curl -L "https://github.com/sohwaje/ncloud_terraform/blob/master/mysql-connector-java-8.0.21.jar?raw=true" -o "/home/$TOMCAT_USER/$CATALINA_HOME_NAME/lib/mysql-connector-java-8.0.21.jar"
+
+# Change permission tomcat Directory
+sudo chown -R $TOMCAT_USER:$TOMCAT_USER /home/$TOMCAT_USER
+
+# add systemctl tomcat service
+sudo bash -c "cat << EOF > /etc/systemd/system/tomcat.service
+[Unit]
+Description=tomcat7
+After=network.target syslog.target
+
+[Service]
+Type=forking
+User=$TOMCAT_USER
+Group=$TOMCAT_USER
+ExecStart=/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/startup.sh
+ExecStop=/home/$TOMCAT_USER/$CATALINA_BASE_NAME/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+# Create test index.jsp
+echo "NCloud TEST PAGE-$HOSTNAME" | sudo tee -a "/home/$TOMCAT_USER/$SOURCE_DIR/$CATALINA_BASE_NAME/index.jsp"
+
+# tomcat start
+sudo systemctl daemon-reload && sudo systemctl start tomcat && sudo systemctl enable tomcat
